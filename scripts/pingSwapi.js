1e329c0eaa81d87cb494ac9daf20f1bb11a52655
MahedialiBhatt <bhattmahediali92@gmail.com>
2023-12-21 10:54:41 +0530

    Added env variable to control cron-job

--- scripts/pingSwapi.js
+++ scripts/pingSwapi.js
@@ -1,16 +1,15 @@
-const axios = require("axios");
 const cron = require("node-cron");
-
-// SWAPI API endpoint
-const swapiApiUrl = "https://swapi.dev/api/people/";
+const { fetchAndCacheAllData } = require("../models/swapiModel");
 
 function swapiCron() {
-  // Cron job to ping SWAPI API every 15 minutes (adjust the cron expression as needed)
-  cron.schedule("*/15 * * * *", async () => {
+  const cronIntervalInMinutes = process.env.CRON_INTERVAL_MINUTES || 30; // Default: every 30 minutes
+
+  // Convert minutes to cron expression
+  const cronExpression = `*/${cronIntervalInMinutes} * * * *`;
+  // Cron job will call SWAPI API every cronIntervalInMinutes
+  cron.schedule(cronExpression, async () => {
     try {
-      // Ping the SWAPI API
-      const response = await axios.get(swapiApiUrl);
-      console.log(`SWAPI /people API pinged successfully at ${new Date()}`);
+      fetchAndCacheAllData();
     } catch (error) {
       console.error("Error pinging SWAPI API:", error.message);
     }
