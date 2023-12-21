91cc3754ac2b0c3408a1f479de315cf338d0cca5
MahedialiBhatt <bhattmahediali92@gmail.com>
2023-12-21 11:07:39 +0530

    Improved logging

--- scripts/pingSwapi.js
+++ scripts/pingSwapi.js
@@ -4,16 +4,22 @@ const { fetchAndCacheAllData } = require("../models/swapiModel");
 function swapiCron() {
   const cronIntervalInMinutes = process.env.CRON_INTERVAL_MINUTES || 30; // Default: every 30 minutes
 
+  console.log(
+    `CRON-JOB: Started - Running every ${cronIntervalInMinutes} minutes`
+  );
+
   // Convert minutes to cron expression
   const cronExpression = `*/${cronIntervalInMinutes} * * * *`;
   // Cron job will call SWAPI API every cronIntervalInMinutes
   cron.schedule(cronExpression, async () => {
     try {
-      fetchAndCacheAllData();
+      await fetchAndCacheAllData();
+      console.log("CRON-JOB: Task completed successfully");
     } catch (error) {
       console.error("Error pinging SWAPI API:", error.message);
     }
   });
+  console.log("CRON-JOB: Ended");
 }
 
 module.exports = { swapiCron };
